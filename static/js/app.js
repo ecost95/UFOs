ddf4e7ccfcc472b552257edb3afee2c2444d0db8
Emily Costello <eccostello1@gmail.com>
2022-11-29 15:22:32 -0500

    final

--- static/js/app.js
+++ static/js/app.js
@@ -7,21 +7,22 @@ var tbody = d3.select("tbody");
 function buildTable(data) {
     // First, clear table body
     tbody.html("");
-}
-// Next, loop through each object in the data
-// and append a row and cells for each value in the row
-data.forEach((dataRow) => {
-    // Append a row to the table body
-    let row = tbody.append("tr");
 
-    //Loop through a field in the dataRow and add each
-    //value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-        }
-    );
-});
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        // Append a row to the table body
+        let row = tbody.append("tr");
+
+        //Loop through a field in the dataRow and add each
+        //value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            
+        });
+    });
+}
 
 function handleClick() {
   // Grab the datetime value from the filter
@@ -40,4 +41,8 @@ function handleClick() {
   // @NOTE: If no date was entered, then filteredData will
   // just be the original tableData.
   buildTable(filteredData);
-};
+}
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
